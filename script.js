d3a3a4d03a99c8952df1d741f76bf0c7d4bcaf00
Akash Deb Nath <akash.dns12345@gmail.com>
2021-02-08 19:46:39 +0600

    updated

--- script.js
+++ script.js
@@ -38,16 +38,18 @@ const renderMealInfo = meal => {
     mealDetail.style.display = "block";
     mealDetail.innerHTML =
         `<img src="${meal.strMealThumb}">
-        <ul>
-        <li>${meal.strMeasure1}${meal.strIngredient1}</li>
-        <li>${meal.strMeasure2}${meal.strIngredient2}</li>
-        <li>${meal.strMeasure3}${meal.strIngredient3}</li>
-        <li>${meal.strMeasure4}${meal.strIngredient4}</li>
-        <li>${meal.strMeasure5}${meal.strIngredient5}</li>
-        <li>${meal.strMeasure6}${meal.strIngredient6}</li>
-        <li>${meal.strMeasure7}${meal.strIngredient7}</li>
-        <li>${meal.strMeasure8}${meal.strIngredient8}</li>
-        <li>${meal.strMeasure9}${meal.strIngredient9}</li>
-        <li>${meal.strMeasure10}${meal.strIngredient10}</li>
-      </ul>`;
+        <ul id = "ul">
+       
+        </ul>`;
+    for (let i = 1; i < 20; i++) {
+        const ul = document.getElementById("ul");
+        let li = document.createElement("li");
+        li.innerText = meal[`strMeasure${i}`] + ' ' + meal[`strIngredient${i}`];
+        if (meal[`strIngredient${i}`]) {
+            ul.appendChild(li);
+        }
+        else {
+            break;
+        }
+    }
 }
